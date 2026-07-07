00eb599b9866ac500e6ee7180f092ead892836cb
Hussenet Thomas <113238884+LeJamon@users.noreply.github.com>
2026-07-07 18:09:07 +0200

    fix: dedup declarations inherited from pre-fix v3.0.0 merge

--- internal/tx/types.go
+++ internal/tx/types.go
@@ -88,16 +88,6 @@ const (
 	TypeLoanManage                   = protocol.TxTypeLoanManage
 	TypeLoanPay                      = protocol.TxTypeLoanPay
 
-	TypeLoanBrokerSet           = protocol.TxTypeLoanBrokerSet
-	TypeLoanBrokerDelete        = protocol.TxTypeLoanBrokerDelete
-	TypeLoanBrokerCoverDeposit  = protocol.TxTypeLoanBrokerCoverDeposit
-	TypeLoanBrokerCoverWithdraw = protocol.TxTypeLoanBrokerCoverWithdraw
-	TypeLoanBrokerCoverClawback = protocol.TxTypeLoanBrokerCoverClawback
-	TypeLoanSet                 = protocol.TxTypeLoanSet
-	TypeLoanDelete              = protocol.TxTypeLoanDelete
-	TypeLoanManage              = protocol.TxTypeLoanManage
-	TypeLoanPay                 = protocol.TxTypeLoanPay
-
 	TypeAmendment = protocol.TxTypeAmendment
 	TypeFee       = protocol.TxTypeFee
 	TypeUNLModify = protocol.TxTypeUNLModify
