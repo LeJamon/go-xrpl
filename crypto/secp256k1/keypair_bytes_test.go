222bcd5edcad122ddfeb748825697484aab475db
LeJamon <113238884+LeJamon@users.noreply.github.com>
2026-09-03 11:50:55 +0200

    test: pin validator key derivation vector

--- crypto/secp256k1/keypair_bytes_test.go
+++ crypto/secp256k1/keypair_bytes_test.go
@@ -1,11 +1,13 @@
-package secp256k1
+package secp256k1_test
 
 import (
 	"encoding/hex"
 	"strings"
 	"testing"
 
+	"github.com/LeJamon/go-xrpl/codec/addresscodec"
 	rootcrypto "github.com/LeJamon/go-xrpl/crypto"
+	"github.com/LeJamon/go-xrpl/crypto/secp256k1"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,16 +15,32 @@ func TestDeriveKeypairBytesMatchesHexAPI(t *testing.T) {
 	seed := []byte{229, 81, 182, 134, 131, 220, 192, 126, 133, 114, 150, 132, 140, 237, 222, 196}
 	for _, validator := range []bool{false, true} {
 		t.Run(map[bool]string{false: "account", true: "validator"}[validator], func(t *testing.T) {
-			privateKey, publicKey, err := (Algorithm{}).DeriveKeypairBytes(seed, validator)
+			privateKey, publicKey, err := (secp256k1.Algorithm{}).DeriveKeypairBytes(seed, validator)
 			require.NoError(t, err)
 			defer rootcrypto.SecureErase(privateKey)
 			require.Len(t, privateKey, 32)
 			require.Len(t, publicKey, 33)
 
-			privateHex, publicHex, err := (Algorithm{}).DeriveKeypair(seed, validator)
+			privateHex, publicHex, err := (secp256k1.Algorithm{}).DeriveKeypair(seed, validator)
 			require.NoError(t, err)
 			require.Equal(t, privateHex, "00"+strings.ToUpper(hex.EncodeToString(privateKey)))
 			require.Equal(t, publicHex, strings.ToUpper(hex.EncodeToString(publicKey)))
 		})
 	}
+}
+
+func TestDeriveKeypairBytesValidatorVector(t *testing.T) {
+	seed := []byte{229, 81, 182, 134, 131, 220, 192, 126, 133, 114, 150, 132, 140, 237, 222, 196}
+	privateKey, publicKey, err := (secp256k1.Algorithm{}).DeriveKeypairBytes(seed, true)
+	require.NoError(t, err)
+	defer rootcrypto.SecureErase(privateKey)
+
+	require.Equal(t,
+		"pnen77YEeUd4fFKG7iycBWcwKpTaeFRkW2WFostaATy1DSupwXe",
+		addresscodec.Base58CheckEncode(privateKey, addresscodec.NodePrivateKeyPrefix),
+	)
+	require.Equal(t,
+		"n94a1u4jAz288pZLtw6yFWVbi89YamiC6JBXPVUj5zmExe5fTVg9",
+		addresscodec.Base58CheckEncode(publicKey, addresscodec.NodePublicKeyPrefix),
+	)
 }
