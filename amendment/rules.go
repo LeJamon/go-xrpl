813c903f28d2eb80e34c074104f59588d72be60a
LeJamon <113238884+LeJamon@users.noreply.github.com>
2026-09-10 15:24:52 +0200

    chore: remove redundant amendment comment
    Remove the introduced doc comment that repeats the cleanup amendment accessor name. No behavior change.

--- amendment/rules.go
+++ amendment/rules.go
@@ -212,7 +212,6 @@ func (r *Rules) FixCleanup3_3_0Enabled() bool {
 	return r.Enabled(FeatureFixCleanup3_3_0)
 }
 
-// FixCleanup3_4_0Enabled reports whether these rules activate the cleanup amendment.
 func (r *Rules) FixCleanup3_4_0Enabled() bool {
 	return r.Enabled(FeatureFixCleanup3_4_0)
 }
